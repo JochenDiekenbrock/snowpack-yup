fac328f0decad27a75b00a45c18e40a358a65de3
Jochen Diekenbrock <jochen.diekenbrock@tui.com>
2021-03-08 09:01:59 +0100

    add yup example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { object, string } from 'yup';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const mySchema = object().shape({
+    type: string().required("type required"),
+  });
+
   // Create the count state.
   const [count, setCount] = useState(0);
   // Create the counter (+1 every second).
